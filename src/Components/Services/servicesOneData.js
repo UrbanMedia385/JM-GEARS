74c1a11f12cd357c07b488f6d11d3b91cfbe62f4
rocky <d369909121@gmail.com>
2025-09-08 17:35:46 +0530

    services

--- src/Components/Services/servicesOneData.js
+++ src/Components/Services/servicesOneData.js
@@ -1,5 +1,12 @@
 import {v4 as uuidv4} from 'uuid';
-import bg1 from '../../assets/img/services/1.jpg'
+import bg1 from '../../assets/img/services/1.png'
+import bg2 from '../../assets/img/services/2.png'
+import bg3 from '../../assets/img/services/3.png'
+import bg4 from '../../assets/img/services/4.png'
+import bg5 from '../../assets/img/services/5.png'
+import bg6 from '../../assets/img/services/6.png'
+
+
 
 const ProductsOneData = [
     {
@@ -12,7 +19,7 @@ const ProductsOneData = [
 
     {
         id: uuidv4(),
-        bg: bg1,
+        bg: bg2,
         icon: 'interior-design',
         title: 'Sugar Industry',
         desc: 'In the sugar sector, we provide critical components such as gears, pinions, and mill heads. ',
@@ -20,7 +27,7 @@ const ProductsOneData = [
 
     {
         id: uuidv4(),
-        bg: bg1,
+        bg: bg3,
         icon: 'home',
         title: 'Chemical Industry',
         desc: 'For the chemical industry, We delivers customised heavy engineering solutions. ',
@@ -28,7 +35,7 @@ const ProductsOneData = [
 
     {
         id: uuidv4(),
-        bg: bg1,
+        bg: bg4,
         icon: 'varnish',
         title: 'Sponge iron Industry',
         desc: 'We play a vital role in the sponge iron industry by providing robust kiln shells, gears, and rollers.',
@@ -36,14 +43,14 @@ const ProductsOneData = [
 
     {
         id: uuidv4(),
-        bg: bg1,
+        bg: bg5,
         icon: 'sketch-1',
         title: 'Tyre Industry',
         desc: 'In the tire industry, we offer specialized components such as gears, pinions, and shafts. ',
     },
     {
         id: uuidv4(),
-        bg: bg1,
+        bg: bg6,
         icon: 'sketch-1',
         title: 'Steel Industry',
         desc: 'JM Gears & Engineering is a trusted partner in the steel sector, providing a range of heavy engineering products. ',
--- src/Constants/BallMill.jsx
+++ src/Constants/BallMill.jsx
@@ -4,7 +4,7 @@ export const BallMill = () => (
 <h3>Ball Mill</h3>
 <ul>
     <li>A Ball Mill is a horizontal rotating cylinder filled with steel or ceramic balls used for grinding various substances.</li>
-    <li>Its rotational motion ensures effective blending and grinding of materials, making it indispensable in industries like cement, minerals, and chemical processing.</li>
+    <li>Its rotational motion ensures effective blending and grinding of materials, making it indispensable in  like cement, minerals, and chemical processing.</li>
     <li>The balls inside the cylinder impact and grind the materials, leading to particle size reduction and homogenization of blends.</li>
     <li>Ball mills are versatile machines capable of grinding materials of various hardness levels, from soft and brittle materials to hard and abrasive substances.</li>
     <li>They are widely used in mineral processing, cement production, and pharmaceutical industries, among others.</li>
