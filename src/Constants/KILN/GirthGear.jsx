fd9afa362074befad9abf4880c1af96752fbfecf
rocky <d369909121@gmail.com>
2025-09-10 13:10:45 +0530

    kilnshell-add

--- src/Constants/KILN/GirthGear.jsx
+++ src/Constants/KILN/GirthGear.jsx
@@ -1,9 +1,10 @@
 export const GirthGear = () => (
   <>
-   <h3>Girth Gear:</h3>
+   <h3>Efficient Girth Gears for Rotary Kilns</h3>
 <ul>
-    <li>Girth gears are essential components that transmit torque from the kiln drive to the rotating kiln, crucial for precise movement in cement and steel production.</li>
-    <li>They provide the necessary power for the rotation of the kiln.</li>
+    <li>Our heavy-duty girth gears are efficient in streamlining the cement and mining industry. With this high-quality power transmission system, you can run your kilns or cement mills without interruption.</li>
+    <li>Known as a crucial component for rotary mills, our precise manufacturing processes ensure robust, noiseless, and girth gears. Crafted with iron cast steel, our product sustains heavy pressure and complies with essential inspections. </li>
+    <li>At JM Gears, we aim to provide reliable product that supports a longer lifespan. In addition, the easy installation, quality control process, and affordable girth gear make us one of the best<strong> cement and steel parts suppliers. </strong>Request a product demo or its price to make an informed decision.</li>
 </ul>
   </>
 );
--- src/Constants/KILN/KilnShell.jsx
+++ src/Constants/KILN/KilnShell.jsx
@@ -1,14 +1,13 @@
 export const KilnShell = () => (
   <>
-    <h3>Kiln Shell</h3>
+    <h3>High-Quality Kiln Shell </h3>
     <ul>
       <li>
-        The kiln shell is the outermost structure of a rotary kiln, providing
-        structural integrity for high-temperature processes.
+      Our premium kiln shell is a large, cylindrical steel component on the outside of the rotary kiln. As the main body of the kiln, it secures the internal lining from outside elements. During the heating, drying or sintering processes, the shell maintains its own and the raw material’s weight. It, in addition, handles the thermal expansion during the entire process. 
       </li>
       <li>
-        It's designed to withstand thermal stresses and mechanical loads while
-        containing the materials being processed inside the kiln.
+      At JM Gears, a premium cement and steel plant manufacturer, we build our shells in sections, which makes them easier to install and repair. Investing in our kiln shells means choosing a reliable and long-lasting solution that protects your assets and boosts overall plant productivity.
+
       </li>
     </ul>
   </>
